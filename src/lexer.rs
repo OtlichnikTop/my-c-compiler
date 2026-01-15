122e8308add4674699bad2240321df1c5dd2d773
OtlichnikTop <bobrurulyat.1999@gmail.com>
2026-01-15 17:54:24 +0700

    Added lexer.
    Uuuhhh... this commit is sooo big...

---
+++ src/lexer.rs
@@ -0,0 +1,270 @@
+use std::fmt;
+
+#[derive(Debug, Clone)]
+pub enum LexerError {
+    UnclosedStringLiteral,
+    UnknownEscapeSequence(String),
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub enum TokenLiteral {
+    Int(i32),        // 123
+    Float(f32),      // 45.32f
+    Char(char),      // 'a'
+    String(String),  // "Hello, World!"
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub enum TokenSeparator {
+    OParen,          // (
+    CParen,          // )
+    OCurly,          // {
+    CCurly,          // }
+    Colon,           // ,
+    SemiColon,       // ;
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub enum TokenOperator {
+    Plus,            // +
+    Minus,           // -
+    Multiply,        // *
+    Divide,          // /
+    Mod,             // %
+    And,             // &
+    Or,              // |
+    Xor,             // ^
+    ShiftLeft,       // <<
+    ShiftRight,      // >>
+    Equal,           // =
+    EqualEqual,      // ==
+    NotEqual,        // !=
+    Less,            // <
+    LessEqual,       // <=
+    Greater,         // >
+    GreaterEqual,    // >=
+    AndAnd,          // &&
+    OrOr,            // ||
+    PlusPlus,        // ++
+    MinusMinus,      // --
+    PlusEqual,       // +=
+    MinusEqual,      // -=
+    MultiplyEqual,   // *=
+    DivideEqual,     // /=
+    ModEqual,        // %=
+    OrEqual,         // |=
+    XorEqual,        // ^=
+    ShiftLeftEqual,  // <<=
+    ShiftRightEqual, // >>=  `ShREq` operator :)
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub enum Token {
+    EOF,
+    Literal(TokenLiteral),
+    Operator(TokenOperator),
+    Separator(TokenSeparator),
+    ID(String),
+}
+
+#[derive(Debug, Clone)]
+pub struct Location {
+    pub filepath: String,
+    pub row: usize,
+    pub col: usize,
+}
+
+impl fmt::Display for Location {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        write!(f, "{}:{}:{}", self.filepath, self.row + 1, self.col + 1)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Lexer {
+    source: String,
+    filepath: String,
+
+    cur: usize, // Cursor
+    row: usize, // Current row
+    bol: usize, // Start of current row
+}
+
+impl Lexer {
+    pub fn new(source: String, filepath: String) -> Self {
+        Self {
+            source,
+            filepath,
+            cur: 0,
+            row: 0,
+            bol: 0,
+        }
+    }
+
+    pub fn get_token(&mut self) ->Result<Token, LexerError> {
+        self.trim_left();
+        if self.is_empty() { return Ok(Token::EOF); }
+
+        let cur_char = self.get_char(0).unwrap();
+        
+        if cur_char.is_alphabetic() {
+            let start: usize = self.cur;
+            while !self.is_empty() && self.get_char(0).unwrap().is_alphanumeric() {
+                self.chop_char();
+            }
+
+            return Ok(Token::ID(self.source[start..self.cur].to_string()))
+        }
+
+        if cur_char.is_ascii_digit() {
+            let start = self.cur;
+            while !self.is_empty() && self.get_char(0).unwrap().is_ascii_digit() {
+                self.chop_char();
+            }
+            let value: i32 = self.source[start..self.cur].parse().unwrap();
+            return Ok(Token::Literal(TokenLiteral::Int(value)));
+        }
+
+        if cur_char == '"' {
+            use std::vec::Vec;
+            self.chop_char();
+            let mut string: Vec<char> = Vec::new();
+            loop {
+                if self.is_empty() { return Err(LexerError::UnclosedStringLiteral); }
+                let ch = self.get_char(0).unwrap();
+                if ch == '"' { break; }
+
+                if ch == '\\' {
+                    if self.is_empty() { return Err(LexerError::UnclosedStringLiteral); }
+                    self.chop_char();
+                    string.push(match self.get_char(0).unwrap() {
+                        // TODO: add support for \nnn, \xhh…, \uhhhh, \Uhhhhhhhh
+                        // https://en.wikipedia.org/wiki/Escape_sequences_in_C#Escape_sequences
+                        'a' => 0x07 as char, // Alert (Beep, Bell) - Added in C89
+                        'b' => 0x08 as char, // Backspace
+                        'e' => 0x1B as char, // Escape character
+                        'f' => 0x0C as char, // Formfeed Page Break
+                        'v' => 0x0B as char, // Vertical Tab
+                        
+                        '?' => '?',  // Question mark (used to avoid trigraphs) https://en.wikipedia.org/wiki/Digraphs_and_trigraphs_(programming)#C
+                        'n' => '\n', // Newline
+                        'r' => '\r', // Carriage Return
+                        't' => '\t', // Horizontal Tab
+                        
+                        '\'' => '\'',
+                        '"' => '"',
+                        '\\' => '\\',
+                        _ => return Err(
+                            LexerError::UnknownEscapeSequence(format!("\\{}", self.get_char(0).unwrap()))
+                        ),
+                    });
+                } else {
+                    string.push(ch);
+                }
+                self.chop_char();
+            }
+            self.chop_char();
+            return Ok(Token::Literal(TokenLiteral::String(string.into_iter().collect::<String>())));
+        }
+
+        self.chop_char();
+
+        return Ok(match cur_char {
+            '(' => Token::Separator(TokenSeparator::OParen),
+            ')' => Token::Separator(TokenSeparator::CParen),
+            '{' => Token::Separator(TokenSeparator::OCurly),
+            '}' => Token::Separator(TokenSeparator::CCurly),
+            ';' => Token::Separator(TokenSeparator::SemiColon),
+            ',' => Token::Separator(TokenSeparator::SemiColon),
+
+            '=' => {
+                if self.is_empty() || self.get_char(0).unwrap() != '=' {
+                    Token::Operator(TokenOperator::Equal)
+                } else {
+                    Token::Operator(TokenOperator::EqualEqual)
+                }
+            },
+            '+' => {
+                if !self.is_empty() {
+                    match self.get_char(0).unwrap() {
+                        '+' => Token::Operator(TokenOperator::PlusPlus),
+                        '=' => Token::Operator(TokenOperator::PlusEqual),
+                        _   => Token::Operator(TokenOperator::Plus),
+                    }
+                } else {
+                    Token::Operator(TokenOperator::Plus)
+                }
+            },
+            '-' => {
+                if !self.is_empty() {
+                    match self.get_char(0).unwrap() {
+                        '-' => Token::Operator(TokenOperator::MinusMinus),
+                        '=' => Token::Operator(TokenOperator::MinusEqual),
+                        _   => Token::Operator(TokenOperator::Minus),
+                    }
+                } else {
+                    Token::Operator(TokenOperator::Minus)
+                }
+            },
+            '*' => {
+                if self.is_empty() || self.get_char(0).unwrap() != '=' {
+                    Token::Operator(TokenOperator::Multiply)
+                } else {
+                    Token::Operator(TokenOperator::MultiplyEqual)
+                }
+            },
+            '/' => {
+                if self.is_empty() || self.get_char(0).unwrap() != '=' {
+                    Token::Operator(TokenOperator::Divide)
+                } else {
+                    Token::Operator(TokenOperator::DivideEqual)
+                }
+            },
+
+            '%' => {
+                if self.is_empty() || self.get_char(0).unwrap() != '=' {
+                    Token::Operator(TokenOperator::Mod)
+                } else {
+                    Token::Operator(TokenOperator::ModEqual)
+                }
+            }
+            // TODO: add proper error reporting
+            _ => panic!("Unknown symbol {cur_char}"),
+        });
+    }
+
+    pub fn get_location(&self) -> Location {
+        Location { filepath: self.filepath.clone(), row: self.row, col: self.cur - self.bol }
+    }
+
+    fn is_empty(&self) -> bool {
+        self.cur >= self.source.len()
+    }
+
+    fn chop_char(&mut self) {
+        if !self.is_empty() {
+            let c: char = self.get_char(0).unwrap();
+            self.cur += 1;
+            if c == '\n' {
+                self.bol = self.cur;
+                self.row += 1;
+            }
+        }
+    }
+
+    fn trim_left(&mut self) {
+        while !self.is_empty() && self.get_char(0).unwrap().is_whitespace() {
+            self.chop_char();
+        }
+    }
+
+    fn drop_line(&mut self) {
+        while !self.is_empty() && self.get_char(0).unwrap() == '\n' { self.chop_char(); }
+        if !self.is_empty() { self.chop_char(); }
+    }
+
+    fn get_char(&self, index: usize) -> Option<char> {
+        self.source.chars().nth(self.cur + index)
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::fs;
+
+pub mod lexer;
+
+const FILEPATH: &str = "./hw.c";
+
 fn main() {
-    println!("Hello, world!");
+    use lexer as clex;
+    
+    let source_code: String = match fs::read_to_string(FILEPATH) {
+        Ok(content) => content,
+        Err(e) => {
+            panic!("{e}");
+        },
+    };
+
+    let mut lexer: clex::Lexer = clex::Lexer::new(source_code, FILEPATH.to_string());
+    
+    loop {
+        let token = lexer.get_token().unwrap();
+        if token == clex::Token::EOF { break; }
+        println!("{token:?}");
+    }
 }
